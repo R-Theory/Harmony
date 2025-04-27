ca47a2510112fcf2f020e3923605796febf767f9
R-Theory <treese2028@gmail.com>
2025-04-27 12:31:07 -0800

    changes

--- batchs/deploy.bat
+++ batchs/deploy.bat
@@ -1,14 +1,6 @@
 @echo off
-REM Prompt for a commit message
 set /p msg="Enter commit message: "
-
-REM Add all changes
 git add .
-
-REM Commit with the provided message
 git commit -m "%msg%"
-
-REM Push to the current branch
 git push
-
 pause
